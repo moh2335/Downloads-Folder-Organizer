00e038e0f0baafc12804ffb567c4c4332791f3ec
Mohammed Khan <mohammedkhan2336@gmail.com>
2021-02-01 13:38:13 +0100

    Done

--- main.py
+++ main.py
@@ -23,27 +23,24 @@ def nav_folder():
 
 def file_move():
     for filename in os.listdir(downloads_path):
-        try:
-            if filename.lower().endswith(('.pdf')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './PDFs' + './' + filename )
-            elif filename.lower().endswith(('.exe', '.msi')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Executables' + './' + filename )
-            elif filename.lower().endswith(('.txt', '.doc', '.docx', '.xls', '.xlsx', '.ods')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Text' + './' + filename )
-            elif filename.lower().endswith(('.zip', '.rar', '.7z')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './ZIP' + './' + filename )
-            elif filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Images' + './' + filename )
-            elif filename.lower().endswith(('.MP4', '.AVI', '.MOV', '.mp4')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Video' + './' + filename )
-            elif filename.lower().endswith(('.MP3', '.mp3')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Audio' + './' + filename )
-            elif filename.lower().endswith(('.torrent')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Torrents' + './' + filename )
-            elif filename.lower().endswith(('.pptx', '.ppt')):
-                shutil.move(downloads_path + './' + filename, downloads_path + './Slides' + './' + filename )
-        except FileNotFoundError:
-            print("The system could not find the specified path for %s, please check again." % filename)
+        if filename.lower().endswith(('.pdf')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './PDFs' + './' + filename )
+        elif filename.lower().endswith(('.exe', '.msi')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Executables' + './' + filename )
+        elif filename.lower().endswith(('.txt', '.doc', '.docx', '.xls', '.xlsx', '.ods')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Text' + './' + filename )
+        elif filename.lower().endswith(('.zip', '.rar', '.7z')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './ZIP' + './' + filename )
+        elif filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Images' + './' + filename )
+        elif filename.lower().endswith(('.MP4', '.AVI', '.MOV', '.mp4')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Video' + './' + filename )
+        elif filename.lower().endswith(('.MP3', '.mp3')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Audio' + './' + filename )
+        elif filename.lower().endswith(('.torrent')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Torrents' + './' + filename )
+        elif filename.lower().endswith(('.pptx', '.ppt')):
+            shutil.move(downloads_path + './' + filename, downloads_path + './Slides' + './' + filename )
 
 if __name__ == "__main__":
     OScheck()
