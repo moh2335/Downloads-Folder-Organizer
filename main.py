0e829ec9e32a9d900d8296379c4fcac69ee48a36
Mohammed Khan <mohammedkhan2336@gmail.com>
2021-02-01 14:34:25 +0100

    Done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import shutil, os
 from pathlib import Path
 from sys import platform
 
+""" DISCLAIMER: I am not responsible for any files aciddentally lost/deleted. Only run this script on files you have a back up for or can redownload! """
+
 folders = ['Executables', 'PDFs', 'Video', 'Other', 'ZIP', 'Images', 'Text', 'Audio', 'Torrents', 'Slides']
 downloads_path = str(Path.home() / "Downloads")
 
@@ -18,7 +20,7 @@ def nav_folder():
             except OSError:
                 print("Creation of the folder %s failed" % folder)
             else:
-                print("%s folder succesfully created" % folder)
+                print("%s folder succesfully created" % folder)          
     file_move_category()
 
 def file_move_category():
@@ -41,14 +43,15 @@ def file_move_category():
             shutil.move(downloads_path + './' + filename, downloads_path + './Torrents' + './' + filename )
         elif filename.lower().endswith(('.pptx', '.ppt')):
             shutil.move(downloads_path + './' + filename, downloads_path + './Slides' + './' + filename )
-        target_dir = os.path.join(downloads_path, '.\Other')
-        file_move_remaining(target_dir)
+        else:
+            target_dir = os.path.join(downloads_path, '.\Other')
+            file_move_remaining(target_dir)
 
 # Move remaining files to the 'Other' folder
 def file_move_remaining(target_dir):
     for filename in os.listdir(downloads_path):
         if os.path.isfile(os.path.join(downloads_path, filename)) == True:
-            shutil.move(os.path.join(downloads_path, filename), target_dir)
+            shutil.move(os.path.join(downloads_path, filename), os.path.join(target_dir, filename))
 
 if __name__ == "__main__":
     OScheck()
